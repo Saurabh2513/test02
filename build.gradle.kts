bd4dda68366cf2c338147a5c0710986ed93a6dcd
sagar <sagaryadav12369@gmail.com>
2025-01-06 14:22:41 +0530

    today

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    id("com.google.devtools.ksp") version "1.8.10-1.0.9" apply false
+}
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath (libs.gradle)
+        classpath (libs.kotlin.gradle.plugin)
+    }
+}
